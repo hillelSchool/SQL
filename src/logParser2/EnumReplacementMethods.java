2aef871d74f84e1c3c78c9e235d900033d9ef525
Denis <denissaranuk@gmail.com>
2014-12-04 11:40:08 +0200

    ver2.3.1 change the code to the factory random methods

---
+++ src/logParser2/EnumReplacementMethods.java
@@ -0,0 +1,8 @@
+package logParser2;
+
+public interface EnumReplacementMethods {
+    static String IPv4 = "IPv4";
+    static String IPv6 = "IPv6";
+    static String Domain = "Domain";
+    static String Email = "Email";
+}
--- src/logParser2/ReplacementMethods.java
+++ src/logParser2/ReplacementMethods.java
@@ -2,17 +2,19 @@ package logParser2;
 
 public class ReplacementMethods {
 
-    public static GetReplacementMethod getReplacementMethod(String method) {
+    public static GetReplacementMethod getReplacementMethod(String method) throws IllegalArgumentException {
 
-        GetReplacementMethod getReplacementMethod = null;
-        if (method.equals("IPv4")) {
+        GetReplacementMethod getReplacementMethod;
+        if (method.equals(EnumReplacementMethods.IPv4)) {
             getReplacementMethod = new GetRandomIPv4();
-        } else if (method.equals("IPv6")) {
+        } else if (method.equals(EnumReplacementMethods.IPv6)) {
             getReplacementMethod = new GetRandomIPv6();
-        } else if (method.equals("Domain")) {
+        } else if (method.equals(EnumReplacementMethods.Domain)) {
             getReplacementMethod = new GetRandomDomain();
-        } else if (method.equals("Email")){
+        } else if (method.equals(EnumReplacementMethods.Email)) {
             getReplacementMethod = new GetRandomEmail();
+        } else {
+            throw new IllegalArgumentException("Unknown method type");
         }
         return getReplacementMethod;
     }
